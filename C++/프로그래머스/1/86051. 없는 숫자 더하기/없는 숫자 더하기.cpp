e5bac777b3b64e3951edc168784902650603ab1c
lKAEDEl <hun151800@gmail.com>
2026-01-27 09:12:03 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int total = 45;
+    int sum = 0;
+    
+    for(int i = 0; i < numbers.size(); i++){
+        sum += numbers[i];
+    }
+    
+    return total - sum;
+}
